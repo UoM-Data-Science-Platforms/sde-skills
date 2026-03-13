f63ababf9e7bf7fcff08d49b4082040a5f29f363
Edd Tempest <edward.tempest@manchester.ac.uk>
2026-03-13 15:44:50 +0000

    fixed image sizes and markdown boing rendered as plain text

--- astro-app/astro.config.mjs
+++ astro-app/astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import remarkMkdocsAttr from './src/plugins/remark-mkdocs-attr.js';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  markdown: {
+    remarkPlugins: [remarkMkdocsAttr]
+  }
 });
